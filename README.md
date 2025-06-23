8b4befa233a7db40b3f9f686b41578c18c6a20c2
designersanket <sanketjagadale04@gmail.com>
2025-06-23 09:38:03 +0530

    Portfolio

---
+++ README.md
@@ -0,0 +1,22 @@
+💼 Personal Portfolio Website
+A responsive and visually engaging personal portfolio website built to showcase my projects, skills, and background in web development and design. It includes interactive UI elements, smooth animations, and a modern design that reflects my personality and professional journey.
+
+✨ Features
+🔹 Home, Projects, Skills, and About sections
+
+🔹 Interactive UI with glowing hover effects
+
+🔹 Responsive layout for all screen sizes
+
+🔹 Project cards with live demo links and screenshots
+
+🔹 Smooth scroll navigation
+
+🔹 Footer with social links and copyright
+
+🛠️ Tech Stack
+Frontend: HTML, CSS, JavaScript
+
+UI Design: Custom CSS Animations & Flexbox
+
+Hosting: Vercel / GitHub Pages.
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect,flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key="dnkvjhrbhvjbdlkjvdvnhgflkjbdvkdjvlknjvdsnkvrugryteincdkjsroihgiurvnrvorhgrevjbhbrvkervnmvamsdvkrbvj"
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Sanket8787'
+app.config['MYSQL_DB'] = 'portfolio'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        message = request.form['textarea']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contacts (email, message) VALUES (%s, %s)", (email, message))
+        mysql.connection.commit()
+        cur.close()
+        flash('Thank You! Your message has been sent.','success')
+        return redirect('/')
+    return render_template('index.html')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+if __name__ == '__main__':
+    app.run(debug=True)

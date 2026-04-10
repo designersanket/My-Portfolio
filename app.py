68b7cafc43060e62fa633072840c348842b1f7dc
designersanket <sanketjagadale04@gmail.com>
2026-04-10 21:38:50 +0530

    working of resume btn

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from pathlib import Path
 
 app = Flask(__name__)
+BASE_DIR = Path(__file__).resolve().parent
 
 @app.route('/')
 def index():
@@ -9,6 +11,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/resume')
+def download_resume():
+    return send_from_directory(BASE_DIR, 'resume.pdf', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
